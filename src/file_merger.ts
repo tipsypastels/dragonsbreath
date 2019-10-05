597500ddef75fd5143ed1459c28366ca30af41b1
tipsypastels <dakotasankey@gmail.com>
2019-10-05 11:56:34 -0700

    file merging

---
+++ src/file_merger.ts
@@ -0,0 +1,31 @@
+export default class FileMerger {
+  constructor(
+    private existingScripts: string,
+    private newScripts: string,
+  ) {}
+
+  merge() {
+    const all = { 
+      ...this.extractScriptsToObj(this.existingScripts),
+      ...this.extractScriptsToObj(this.newScripts),
+    };
+
+    const finalScript = Object.entries(all).map(([title, body]) => {
+      return title + body;
+    }).join('\n\n');
+    
+    return finalScript;
+  }
+
+  private extractScriptsToObj(scripts: string): { [key: string]: string } {
+    const results = {};
+    const titlesAndBodies = scripts.split(/^\s*([A-z0-9_]+::?)/m)
+      .slice(1);
+
+    for (let i = 1; i < titlesAndBodies.length; i += 2) {
+      results[titlesAndBodies[i - 1]] = titlesAndBodies[i];    
+    }
+
+    return results;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import FileReader from './file_reader';
 import Transpiler from './transpiler'
 import parse from './parser';
 import fs from 'fs';
+import FileMerger from './file_merger';
 
 function pipeThroughDragonsbreath(content: string): string {
   return new Transpiler().transpile(parse(content));
@@ -12,9 +13,18 @@ new FileReader().compileAll((file, content) => {
 
   try {
     result = pipeThroughDragonsbreath(content);
-    let resultPath = file.replace(/.dbr$/, '.inc')
-    fs.writeFileSync(resultPath, result);
-    console.log(`Compiled file ${file} to ${resultPath}`);
+    let resultPath = file.replace(/.dbr$/, '.inc');
+
+    if (fs.existsSync(resultPath)) {
+      const existingScripts = fs.readFileSync(resultPath, 'utf8');
+      const merger = new FileMerger(existingScripts, result);
+
+      fs.writeFileSync(resultPath, merger.merge());
+      console.log(`Compiled file ${file} to ${resultPath}, merging with existing scripts`);
+    } else {
+      fs.writeFileSync(resultPath, result);
+      console.log(`Compiled file ${file} to ${resultPath}`);
+    }
   } catch (e) {
     console.log(`\x1b[31mFailed to compile file ${file}. ${e}\x1b[0m`);
   }
